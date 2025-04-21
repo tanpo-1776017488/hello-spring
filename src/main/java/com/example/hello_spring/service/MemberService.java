af1c465615be4ece703f464bf5c27bd09806fed0
Jaehyeon Hwnag <tanpo1733@naver.com>
2025-04-21 23:42:20 +0900

    feature: Add component scan example
    The Controller, Service, and Repository annotations have the Component annotation inside.
    When the @Component annotation is used, Spring registers the class as a bean.
    
    When the @Autowired annotation is used, Spring performs dependency injection automatically.

--- src/main/java/com/example/hello_spring/service/MemberService.java
+++ src/main/java/com/example/hello_spring/service/MemberService.java
@@ -3,10 +3,12 @@ package com.example.hello_spring.service;
 import com.example.hello_spring.domain.Member;
 import com.example.hello_spring.repository.MemberRepository;
 import com.example.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
